fc503b51f3f9039423f5460f7045cecc0ee57dda
user <Thihazawzaw305>
2023-07-27 21:37:11 +0630

    commit

--- app/src/main/java/com/example/videoplayer/MainActivity.kt
+++ app/src/main/java/com/example/videoplayer/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.example.videoplayer
 
+import android.media.Image.Plane
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.SimpleExoPlayer
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.custom_controller.*
+import java.util.SimpleTimeZone
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val simpleExoPlayer = SimpleExoPlayer.Builder(this)
+            .setSeekBackIncrementMs(5000)
+            .setSeekForwardIncrementMs(5000)
+            .build()
+       playerView.player = simpleExoPlayer
+        playerView.keepScreenOn = true
+        simpleExoPlayer.addListener(object : Player.Listener{
+            override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
+                if (playbackState == Player.STATE_BUFFERING){
+                  progressBar.visibility = View.VISIBLE
+                }
+                else  if(playbackState == Player.STATE_READY){
+                    progressBar.visibility = View.GONE
+
+                }
+            }
+        })
+        val videoSource = Uri.parse("http://thinkingform.com/video-sample-mp4/")
+        val mediaItem =  MediaItem.fromUri(videoSource)
+        simpleExoPlayer.setMediaItem(mediaItem)
+        simpleExoPlayer.prepare()
+        simpleExoPlayer.play()
     }
 }
