78972d369abd339826d6a0982d9be7975b09357e
user <Thihazawzaw305>
2023-07-28 09:17:02 +0630

    commit

--- app/src/main/java/com/example/videoplayer/MainActivity.kt
+++ app/src/main/java/com/example/videoplayer/MainActivity.kt
@@ -4,11 +4,13 @@ package com.example.videoplayer
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import androidx.media3.common.MediaItem
 import androidx.media3.common.MimeTypes
+import androidx.media3.common.Player
 import androidx.media3.common.util.Util
 import androidx.media3.exoplayer.ExoPlayer
 import kotlinx.android.synthetic.main.activity_main.*
@@ -18,6 +20,7 @@ class MainActivity : AppCompatActivity() {
     private var playWhenReady = true
     private var currentItem = 0
     private var playbackPosition = 0L
+    private val playbackStateListener : Player.Listener = playbackStatListener()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -64,15 +67,16 @@ class MainActivity : AppCompatActivity() {
             .also { exoPlayer ->
                 playerView.player = exoPlayer
 
-               val mediaItem = MediaItem.fromUri(getString(R.string.test_video))
-
+              val mediaItem = MediaItem.fromUri(getString(R.string.test_video))
+//
 //                val mediaItem = MediaItem.Builder()
-//                    .setUri(getString(R.string.test_video))
+//                    .setUri(getString(R.string.test_video3))
 //                    .setMimeType(MimeTypes.APPLICATION_MPD)
 //                    .build()
                 exoPlayer.setMediaItem(mediaItem)
                 exoPlayer.playWhenReady = playWhenReady
                 exoPlayer.seekTo(currentItem,playbackPosition)
+                exoPlayer.addListener(playbackStateListener)
                 exoPlayer.prepare()
             }
     }
@@ -82,6 +86,7 @@ class MainActivity : AppCompatActivity() {
             playWhenReady = exoPlayer.playWhenReady
             currentItem = exoPlayer.currentMediaItemIndex
             playbackPosition = exoPlayer.currentPosition
+            exoPlayer.removeListener(playbackStateListener)
             exoPlayer.release()
         }
         player = null
@@ -96,5 +101,28 @@ class MainActivity : AppCompatActivity() {
                 WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
         }
 
+    }
+
+
+
+}
+
+private const val TAG = "PlayerActivity"
+private fun playbackStatListener() = object :Player.Listener{
+    override fun onPlaybackStateChanged(playbackState: Int) {
+        val statString : String = when(playbackState){
+            ExoPlayer.STATE_IDLE -> "ExoPlayer.STATE_IDLE"
+            ExoPlayer.STATE_BUFFERING -> "ExoPlayer.STATE_BUFFERING"
+            ExoPlayer.STATE_READY -> "ExoPlayer.STATE_READY"
+            ExoPlayer.STATE_ENDED -> "ExoPlayer.STATE_ENDED"
+
+            else -> "UNKNOWN STATE"
+        }
+        Log.d(TAG,"changed state to $statString")
+    }
+
+    override fun onIsPlayingChanged(isPlaying: Boolean) {
+        val playingString : String = if (isPlaying) "PLAYING" else "NOT PLAYING"
+        Log.d(TAG,"Player is currently $playingString")
     }
 }
